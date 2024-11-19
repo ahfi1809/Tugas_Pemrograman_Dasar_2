05da6941c782409ccd050b519a79a67b71dd6969
Akhmad Fitriannor (Ahfi) <77096009+ahfi1809@users.noreply.github.com>
2024-11-19 23:18:24 +0800

    Add files via upload

---
+++ MuhammadMauladi_2310131110005.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Identitas {
+    protected Scanner miyuki = new Scanner(System.in);
+}
+
+class Nik extends Identitas {
+    public void pekerja() {
+        System.out.print("Masukkan NIK Anda: ");
+        String niki = miyuki.nextLine();
+        System.out.print("Masukkan Nama Anda: ");
+        String name = miyuki.nextLine();
+        System.out.print("Masukkan Tahun Lahir Anda: ");
+        int tahun = miyuki.nextInt();
+        miyuki.nextLine(); 
+        System.out.print("Masukkan Pekerjaan Anda: ");
+        String gawi = miyuki.nextLine();
+
+        System.out.println("\nNIK : " + niki);
+        System.out.println("Nama : " + name);
+        System.out.println("Tahun Lahir : " + tahun);
+        System.out.println("Pensiun pada tahun : " + (tahun + 60));
+        System.out.println("Pekerjaan : " + gawi);
+    }
+}
+
+class Poly {
+    public static void main(String[] args) {
+        Nik mauladi = new Nik();
+        mauladi.pekerja();
+    }
+}
